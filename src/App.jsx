5dba84eb74590fc7a5fb0fab324499ba4e574746
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2024-06-16 01:21:32 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import Profile from "./components/Profile/Profile";
-import userData from "./userData.json";
-import FriendList from "./components/FriendList/FriendList";
-import friends from "./friends.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import transactions from "./transactions.json";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
 
 const App = () => {
   return (
-    <>
-      <Profile
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <p>ContactForm</p>
+      <p>SearchBox</p>
+      <p>ContactList</p>
+    </div>
   );
 };
 
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,1 @@
+import css "./ContactForm.module.css";
